d9da8a900c455345a78ee79aa61dc56d0663fc36
chithuandev <tranchithuanjr@gmail.com>
2025-09-26 00:26:01 +0700

    create docker file

--- ReverseProxy/Program.cs
+++ ReverseProxy/Program.cs
@@ -6,7 +6,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -20,6 +21,8 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
-app.MapControllers();
+app.MapControllers(); 
+
+app.MapReverseProxy();
 
 app.Run();
